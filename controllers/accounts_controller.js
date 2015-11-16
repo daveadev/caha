7ca5c6989d87b51e33bfe5dabbb02e8b32e3e4e8
myteddy15 <lattedphilip@gmail.com>
2015-11-16 16:32:36 +0800

    Add loader

--- controllers/accounts_controller.js
+++ controllers/accounts_controller.js
@@ -2,6 +2,7 @@
 define(['app','api'], function (app) {
     app.register.controller('AccountController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
 		$scope.list=function(){
+			$rootScope.__MODULE_NAME = 'Account';
 			api.GET('accounts',function success(response){
 				console.log(response.data);
 				$scope.Accounts=response.data;	
