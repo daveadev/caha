c0b4180d5c2f3f56e6641e2c8daa02d6a0380107
myteddy15 <lattedphilip@gmail.com>
2015-11-19 14:26:58 +0800

    Add modal to Ledgers

--- controllers/ledgers_controller.js
+++ controllers/ledgers_controller.js
@@ -1,12 +1,15 @@
 "use strict";
 define(['app','api'], function (app) {
-    app.register.controller('LedgerController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
+    app.register.controller('LedgerController',['$scope','$rootScope','$uibModal','api', function ($scope,$rootScope,$uibModal,api) {
 		$scope.list=function(){
 			$rootScope.__MODULE_NAME = 'Ledger';
-			api.GET('ledgers',function success(response){
-				console.log(response.data);
-				$scope.Ledgers=response.data;	
-			});
+			$scope.initLedgers=function(){
+				api.GET('ledgers',function success(response){
+					console.log(response.data);
+					$scope.Ledgers=response.data;	
+				});
+			};
+			$scope.initLedgers();
 			$scope.hasInfo = false;
 			$scope.hasNoInfo = true;
 			$scope.openLedgerInfo=function(ledger){
@@ -28,8 +31,46 @@ define(['app','api'], function (app) {
 			$scope.clearSearch = function(){
 				$scope.searchLedger = null;
 			};
+			$scope.openModal=function(){
+				var modalInstance = $uibModal.open({
+					animation: true,
+					templateUrl: 'myModalContent.html',
+					controller: 'ModalInstanceController',
+				});
+				modalInstance.result.then(function (selectedItem) {
+				  $scope.selected = selectedItem;
+				}, function (source) {
+					if(source==='confirm')
+						$scope.initLedgers();
+				});
+			};
 		};
     }]);
+	app.register.controller('ModalInstanceController',['$scope','$uibModalInstance','api', function ($scope, $uibModalInstance, api){
+		$scope.confirmLedger = function(){
+			 $scope.Ledgers={
+						  account: {
+									account_no:34567,
+									account_name:$scope.accountName,
+									account_type:"student"
+									},
+						  type: $scope.type,
+						  date: $scope.date,
+						  ref_no: $scope.refNo,
+						  details: $scope.details,
+						  amount: $scope.amount
+						 };
+			api.POST('ledgers',$scope.Ledgers,function success(response){
+				$uibModalInstance.dismiss('confirm');
+			});
+		};
+		$scope.cancelLedger = function(){
+			$uibModalInstance.dismiss('cancel');
+		};
+		$scope.setType=function(value){
+			$scope.type=value;
+		};
+	}]);
 });
 
 
