c266e360985c862fed9011f18d9184bab1ce656a
myteddy15 <lattedphilip@gmail.com>
2015-11-20 11:31:51 +0800

    Add summary to cashier

--- controllers/cashiers_controller.js
+++ controllers/cashiers_controller.js
@@ -3,8 +3,25 @@ define(['app','api'], function (app) {
     app.register.controller('CashierController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
 		$scope.index=function(){
 			$rootScope.__MODULE_NAME = 'Cashier';
-			$scope.ActiveStep=1;
+			$scope.Steps = [
+				{id:1, title: "Student", description:"Select Student"},
+				{id:2, title: "Transaction", description:"Select Transactions"},
+				{id:3, title: "Payment", description:"Select Payment Methods"},
+				{id:4, title: "Confirmation", description:"Confirmation"}
+			];
+			$scope.initCashier = function(){
+				$scope.ActiveStep=1;
+				$scope.ActiveStudent={};
+				$scope.SelectedStudent={};
+				$scope.ActiveTransactions={};
+				$scope.SelectedTransactions={};
+				$scope.ActivePayments={};
+				$scope.SelectedPayments={};
+				$scope.TotalDue=0;
+				$scope.TotalPaid=0;			
+			};
 			
+			$scope.initCashier();
 			api.GET('students',function success(response){
 				console.log(response.data);
 				$scope.Students=response.data;	
@@ -17,59 +34,57 @@ define(['app','api'], function (app) {
 				console.log(response.data);
 				$scope.Payments=response.data;	
 			});
-			$scope.Steps = [
-				{id:1, title: "Student", description:"Select Student"},
-				{id:2, title: "Transaction", description:"Select Transactions"},
-				{id:3, title: "Payment", description:"Select Payment Methods"},
-				{id:4, title: "Confirmation", description:"Confirmation"}
-			];
-			$scope.SelectedTransactions={};
-			$scope.SelectedPayments={};
+			
 			$scope.nextStep = function(){
-			if($scope.ActiveStep===1){
-				$scope.ActiveStudent = $scope.SelectedStudent;
-				console.log($scope.ActiveStudent);
-			}
-			if($scope.ActiveStep===2){
-				$scope.ActiveTransactions=[];
-				for(var index in $scope.TransactionTypes){
-					var transactionType=$scope.TransactionTypes[index];
-					var transaction = {
-									  id:transactionType.id,
-									  amount:transactionType.amount
-									  };
-					if($scope.SelectedTransactions[transactionType.id]){
-						$scope.ActiveTransactions.push(transaction);
-						};
+				if($scope.ActiveStep===1){
+					$scope.ActiveStudent = $scope.SelectedStudent;
+					console.log($scope.ActiveStudent);
+				}
+				if($scope.ActiveStep===2){
+					$scope.ActiveTransactions=[];
+					$scope.TotalDue = 0;
+					for(var index in $scope.TransactionTypes){
+						var transactionType=$scope.TransactionTypes[index];
+						var transaction = {
+										  id:transactionType.id,
+										  amount:transactionType.amount
+										  };
+						if($scope.SelectedTransactions[transactionType.id]){
+							$scope.TotalDue= $scope.TotalDue + transaction.amount;
+							$scope.ActiveTransactions.push(transaction);
+							console.log($scope.ActiveTransactions);
+							};
+					};
+				}
+				if($scope.ActiveStep===3){
+					$scope.ActivePayments=[];
+					$scope.TotalPaid = 0;
+					for(var index in $scope.Payments){
+						var paymentMethod=$scope.Payments[index];
+						var payment = {
+										  id:paymentMethod.id,
+										  amount:paymentMethod.amount
+										  };
+						if($scope.SelectedPayments[paymentMethod.id]){
+							$scope.TotalPaid = $scope.TotalPaid + payment.amount;
+							$scope.ActivePayments.push(payment);
+							};
+					};
 				};
-			}
-			if($scope.ActiveStep===3){
-				$scope.ActivePayments=[];
-				for(var index in $scope.Payments){
-					var paymentMethod=$scope.Payments[index];
-					var payment = {
-									  id:paymentMethod.id,
-									  amount:paymentMethod.amount
-									  };
-					if($scope.SelectedPayments[paymentMethod.id]){
-						$scope.ActivePayments.push(payment);
-						};
+				if($scope.ActiveStep===4){
+					$scope.Payment={
+									student:$scope.ActiveStudent,
+									transactions:$scope.ActiveTransactions,
+									payments:$scope.ActivePayments,
+								   };
+					api.POST('payments',$scope.Payment,function success(response){
+						console.log(response.data);
+					});
+					
 				};
-			};
-			if($scope.ActiveStep===4){
-				$scope.Payment={
-							    student:$scope.ActiveStudent,
-							    transactions:$scope.ActiveTransactions,
-							    payments:$scope.ActivePayments,
-							   };
-				api.POST('payments',$scope.Payment,function success(response){
-					console.log(response.data);
-				});
-				
-			};
-			if($scope.ActiveStep<$scope.Steps.length){
-				$scope.ActiveStep++;
-			}
+				if($scope.ActiveStep<$scope.Steps.length){
+					$scope.ActiveStep++;
+				}
 			};
 			$scope.prevStep = function(){
 				if($scope.ActiveStep>1){
@@ -102,6 +117,12 @@ define(['app','api'], function (app) {
 			$scope.resetPayments = function(){
 				$scope.SelectedPayments={};
 				$scope.ActivePayments = null;
+			};
+			$scope.filterIncludedTransactions = function(transaction){
+				return $scope.SelectedTransactions[transaction.id] && $scope.ActiveTransactions.length;
+			};
+			$scope.filterIncludedPayments = function(payment){
+				return $scope.SelectedPayments[payment.id] && $scope.ActivePayments.length;
 			};
 		};
     }]);
