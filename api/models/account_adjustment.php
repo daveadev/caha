e189875fc46b972056388dbbce7ff4b7989ce9c7
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-29 16:33:39 +0800

    Update db config for sfm related models

--- api/models/account_adjustment.php
+++ api/models/account_adjustment.php
@@ -1,6 +1,7 @@
 <?php
 class AccountAdjustment extends AppModel {
 	var $name = 'AccountAdjustment';
+	var $useDbConfig = 'sfm';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $belongsTo = array(
