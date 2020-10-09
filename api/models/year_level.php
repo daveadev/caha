4c8ff65f8457acc2addcfbdcb4ecf230b769f826
Dave Alaras <arroyo.daveil@gmail.com>
2020-10-09 10:51:04 +0800

    Update year_level model

--- api/models/year_level.php
+++ api/models/year_level.php
@@ -4,15 +4,7 @@ class YearLevel extends AppModel {
 	var $useDbConfig = 'ser';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $order = 'YearLevel.order';
-	var $belongsTo = array(
-		'EducLevel' => array(
-			'className' => 'EducLevel',
-			'foreignKey' => 'educ_level_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+	
 
 	var $hasMany = array(
 		'Student' => array(
