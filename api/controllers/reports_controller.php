dd18fd1703ca0b952bf5b897aec748c56202b3a5
SteplerPaulo <paulobiscocho@gmail.com>
2020-10-13 20:35:26 +0800

    Fix SOA Bugs
    *ledger query conditions
    *ledger order by

--- api/controllers/reports_controller.php
+++ api/controllers/reports_controller.php
@@ -12,10 +12,11 @@ class ReportsController extends AppController{
 			$student = $this->Student->find('first',array(array('Student.id'=>$account_id)));
 			
 			//Student's SOA
-			$cond =  array('Ledger.account_id'=>$account_id);
-			$order = array('Ledger.transac_date'=>'asc');
-			$data = $this->Ledger->find('all',array($cond,$order));
-			
+			$data = $this->Ledger->find('all',array(
+				'conditions'=>array('Ledger.account_id'=>$account_id),
+				'order'=>'Ledger.transac_date'
+			));
+			//pr($data);exit;
 			
 			$this->set(compact('data','student'));
 		}else{
