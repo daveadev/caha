2fb69945735ecc9198afec4d643465870a810a14
SteplerPaulo <paulobiscocho@gmail.com>
2020-10-27 15:02:42 +0800

    Daily Collection Report

---
+++ api/vendors/daily_collections.php
@@ -0,0 +1,90 @@
+<?php
+require('vendors/fpdf17/formsheet.php');
+class DailyCollections extends Formsheet{
+	protected static $_width = 8.5;
+	protected static $_height = 11;
+	protected static $_unit = 'in';
+	protected static $_orient = 'P';	
+	protected static $curr_page = 1;
+	protected static $page_count;
+	//negative ()
+	function DailyCollections(){
+		$this->showLines = !true;
+		$this->FPDF(DailyCollections::$_orient, DailyCollections::$_unit,array(DailyCollections::$_width,DailyCollections::$_height));
+		$this->createSheet();
+	}
+	
+	
+	function hdr($data){
+		$this->showLines = !true;
+		$metrics = array(
+			'base_x'=> 0.5,
+			'base_y'=> 0.25,
+			'width'=> 7.5,
+			'height'=> 0.8,
+			'cols'=> 38,
+			'rows'=> 4,	
+		);
+		
+		
+		$this->section($metrics);
+		$this->GRID['font_size']=9;
+		$y=1;
+		$this->leftText(0,$y++,'Lake Shore Educational Institution','','');
+		$this->leftText(0,$y++,'Daily Collection Summary Report','','');
+		$this->leftText(0,$y++,'School Year: 2020 - 2021','','');
+		$this->leftText(0,$y++,'Month of: August 2020','','');
+	
+	}
+	
+	function data($data,$total_page,$page){
+		$this->showLines = !true;
+		$metrics = array(
+			'base_x'=> 0.5,
+			'base_y'=> 1.2,
+			'width'=> 7.5,
+			'height'=> 9.4,
+			'cols'=> 15,
+			'rows'=> 47,	
+		);
+		$this->section($metrics);
+		
+		$y=0;
+		$this->drawLine($y++,'h');
+		$this->drawLine($y++,'h');
+	
+	
+	
+		$y=0.8;
+		$this->GRID['font_size']=9;
+		$this->centerText(0,$y,'Date',2,'');
+		$this->centerText(2,$y,'Day',2,'');
+		$this->leftText(4.2,$y,'Description','','');
+		$this->centerText(11,$y,'Collection',2,'');
+		$this->centerText(13,$y++,'Balance',2,'');
+		
+		$totalcollectionperpage = 0;
+		foreach($data as $d){
+			$totalcollectionperpage+= $d['collection'];
+			$this->centerText(0,$y,$d['date'],2,'');
+			$this->centerText(2,$y,$d['day'],2,'');
+			$this->leftText(4.2,$y,ucfirst($d['description']),'','');
+			$this->rightText(12.9,$y,number_format($d['collection'],2),'','');
+			$this->rightText(14.9,$y,number_format($d['balance'],2),'','');
+			$dvr = '---------------------------------------------';
+			$this->leftText(0,$y+0.4,$dvr.$dvr.$dvr.$dvr,'','');
+			$y++;
+		}
+		$this->rightText(11,$y,'Total Collection Per Page:','','');
+		$this->rightText(12.9,$y,number_format($totalcollectionperpage,2),'','');
+		
+		$this->GRID['font_size']=8;
+		$this->leftText(0,48,'Printed by: '.'Cashier 1','','');
+		$this->centerText(0,48,'Date & Time Printed: '. date("M d, Y h:i:s A"),15,'');
+		$this->rightText(14.9,48,'Page '.$page.' of '.$total_page,'','');
+		
+	}
+	
+}
+?>
+	
