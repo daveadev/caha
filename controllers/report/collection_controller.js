d9b0e84dd7da09621d6ee1a093557164d25cd7f7
bienchong <bien_imbang@yahoo.com>
2020-10-28 09:27:57 +0800

    done collection UI adjustments

--- controllers/report/collection_controller.js
+++ controllers/report/collection_controller.js
@@ -33,14 +33,16 @@ define(['app','api','atomic/bomb'],function(app){
 			if(!active) return false;
 			console.log(active);
 			$scope.ActiveSY =  active.sy;
-			getCollections();
+			if($scope.date_to)
+				getCollections();
 		});
 		$scope.setActOption = function(opt){
+			$scope.date_from='';
+			$scope.date_to='';
+			$scope.Collections ='';
 			$scope.ActiveOpt = opt;
 			if(opt.id=='month')
 				getLedgerMonths();
-			if($scope.Collections)
-				getCollections();
 		}
 		$scope.SetActiveMonth = function(mo){
 			$scope.ActiveMonth = mo;
@@ -54,6 +56,32 @@ define(['app','api','atomic/bomb'],function(app){
 		
 		$scope.ChangeDate = function(){
 			$scope.Loaded = 0;
+		}
+		
+		$scope.Clear = function(){
+			$scope.date_from='';
+			$scope.date_to='';
+			$scope.Collections = '';
+		}
+		
+		$scope.setFrom = function(index){
+			var indexno =  $scope.MonthsFilter.indexOf(index);
+			$scope.FilteredMonths = [];
+			if(indexno==$scope.MonthsFilter.length-1)
+				$scope.FilteredMonths.push($scope.MonthsFilter[indexno]);
+			else{
+				for(var i=indexno;i<$scope.MonthsFilter.length;i++){
+					if(i<$scope.MonthsFilter.length)
+						$scope.FilteredMonths.push($scope.MonthsFilter[i]);
+				}
+			}
+		}
+		
+		$scope.NavPage = function(where){
+			if(where=='next')
+				$scope.ActivePage++;
+			else
+				$scope.ActivePage--;
 		}
 		
 		function formatDate(date){
@@ -73,33 +101,83 @@ define(['app','api','atomic/bomb'],function(app){
 				angular.forEach(response.data, function(led){
 					var date = new Date(led.transac_date);
 					var month = date.getMonth();
-					var year = date.getYear();
-					var selection = month+'-'+year;
-					if(Months.indexOf(selection)===-1)
+					month = month+1;
+					var year = date.getFullYear();
+					var selection = month+' '+year;
+					if(Months.indexOf(selection)===-1){
 						Months.push(selection);
+					}
 				});
-				console.log(Months);
+				$scope.MonthsFilter = [];
+				angular.forEach(Months, function(mo){
+					var month = mo.split(' ');
+					var desc = '';
+					switch(parseInt(month[0])){
+						case 1: desc = 'Jan'; break;
+						case 2: desc = 'Feb'; break;
+						case 3: desc = 'Mar'; break;
+						case 4: desc = 'Apr'; break;
+						case 5: desc = 'May'; break;
+						case 6: desc = 'Jun'; break;
+						case 7: desc = 'Jul'; break;
+						case 8: desc = 'Aug'; break;
+						case 9: desc = 'Sep'; break;
+						case 10: desc = 'Oct'; break;
+						case 11: desc = 'Nov'; break;
+						case 12: desc = 'Dec'; break;
+					}
+					$scope.MonthsFilter.push({month:month[0],label:desc+' '+month[1],year:month[1]});
+				});
 			});
 		}
 		
 		function getCollections(){
-			$scope.Collections = '';
-			
+			$scope.Collections = {};
 			var data = {
 				type:$scope.ActiveOpt.id,
 				esp:2020,
 				from:$scope.date_from,
 				to:$scope.date_to
 			};
-			data.from = $filter('date')(new Date(data.from),'yyyy-MM-dd');
-			data.to = $filter('date')(new Date(data.to),'yyyy-MM-dd');
+			if($scope.ActiveOpt.id=='daily'){
+				data.from = $filter('date')(new Date(data.from),'yyyy-MM-dd');
+				data.to = $filter('date')(new Date(data.to),'yyyy-MM-dd');
+			}else{
+				var today = new Date();
+				var currMonth = today.getMonth()+1;
+				if(currMonth==parseInt(data.to.month))
+					$scope.Today = (new Date()).getUTCDate()+' '+data.to.label;
+				data.from = data.from.year+'-'+data.from.month+'-01';
+				data.to = data.to.year+'-'+data.to.month+'-31';
+				
+			}
 			api.GET('collections',data, function success(response){
 				var collection = response.data[0];
 				var total_recvbl = collection.total_receivables-collection.total_subsidies;	
 				collection['cfp'] = (collection.collection_forwarded/total_recvbl)*100;
 				collection['bbp'] = (collection.net_receivables/total_recvbl)*100;
 				$scope.Loaded = 1;
+				
+				if($scope.ActiveOpt.id=='daily'){
+					var i = 0;
+					var ctr = 0;
+					var collections = [];
+					angular.forEach(collection.collections, function(col){
+						if(!collections[i])
+							collections[i]=[];
+						collections[i][ctr] = col;
+						if(ctr==9){
+							i++;
+							ctr = 0;
+						}else
+							ctr++;
+					});
+					$scope.ActivePage = 1;
+					$scope.Modified = collections;
+					collection.collections = collections;
+				}
 				$scope.Collections = collection;
+				console.log($scope.Collections);
 				$scope.Loading = 0;
 			});
 		}
