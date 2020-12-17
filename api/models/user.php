3d509e8de6d6d02ab9141f27f73b1bb9bb497372
bienchong <bien_imbang@yahoo.com>
2020-12-17 17:10:06 +0800

    added cashier data when user is cashier

--- api/models/user.php
+++ api/models/user.php
@@ -3,5 +3,20 @@ class User extends AppModel {
 	var $name = 'User';
 	var $consumableFields = array('id','username','user_type_id','password');
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
-	
+	var $hasMany = array(
+		
+		'Cashier' => array(
+			'className' => 'Cashier',
+			'foreignKey' => 'user_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
 }
