c0e1c1976c79a92fade4608e1b1dc1410c7629bb
bienchong <bienchong@gmail.com>
2022-02-17 13:30:59 +0800

    fixed error looking for subject. cant transac cashier

---
+++ api/models/subject.php
@@ -0,0 +1,9 @@
+<?php
+class Subject extends AppModel {
+	var $name = 'Subject';
+	var $useDbConfig = 'ser';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	
+
+}
