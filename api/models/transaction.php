86777f21460c5f4ceca17471a4fbe24e863b621d
bienchong <bien_imbang@yahoo.com>
2022-07-12 10:45:06 +0800

    corrected condition in getting receipts

--- api/models/transaction.php
+++ api/models/transaction.php
@@ -113,7 +113,7 @@ class Transaction extends AppModel {
 					}
 				}
 				
-			$conds = array('OR'=>array('Transaction.ref_no LIKE'=> '%'.$typ.'%'),'and'=>array('Transaction.transac_date <='=>$end,'Transaction.transac_date >='=>$start));
+			$conds = array('Transaction.ref_no LIKE'=> '%'.$typ.'%','and'=>array('Transaction.transac_date <='=>$end,'Transaction.transac_date >='=>$start));
 				
 			}
 			//pr($conds); exit();
