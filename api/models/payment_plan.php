c4f2c3c9f6e90a07237be392a6b4c53915b32ddb
Dave Arroyo <arroyo.daveil@gmail.com>
2023-09-12 12:14:46 +0800

    Update payment_plan model

--- api/models/payment_plan.php
+++ api/models/payment_plan.php
@@ -2,13 +2,15 @@
 class PaymentPlan extends AppModel {
 	var $name = 'PaymentPlan';
 	var $recursive = 1;
+	var $consumableFields = array('label','id','account_id','guarantor','monthly_payments','total_due','terms');
+	var $virtualFields = array('label'=>"CONCAT(PaymentPlan.guarantor,'/ Php ',PaymentPlan.total_due, ' for ', PaymentPlan.terms, ' mos.' )");
 	var $hasMany = array(
 		'PayPlanSchedule' => array(
 			'className' => 'PayPlanSchedule',
 			'foreignKey' => 'payment_plan_id',
 			'dependent' => false,
 			'conditions' => '',
-			'fields' => '',
+			'fields' => array('id','pay_count','due_date','due_amount','paid_amount','status'),
 			'order' => '',
 			'limit' => '',
 			'offset' => '',
