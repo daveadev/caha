21a2987eb3856de8c5615d07e68cdba39e2815ee
Dave Arroyo <arroyo.daveil@gmail.com>
2023-10-10 09:30:08 +0800

    Handle paysched load and compute OLDAC

--- utils/transactions.js
+++ utils/transactions.js
@@ -2,7 +2,8 @@ define(['app','util','api'],function(app,util) {
 	const TRNX ={};
 	const INI_TRNX = [
 				{id:'INIPY', description:'Initial Payment',amount:0},
-				{id:'SBQPY', description:'Subsequent Payment',amount:0}
+				{id:'SBQPY', description:'Subsequent Payment',amount:0},
+				{id:'OLDAC', description:'Old Account',amount:0}
 			];
 	var api,list,listIndex,paysched;
 	(function(){
@@ -52,39 +53,69 @@ define(['app','util','api'],function(app,util) {
 			
 		}	
 	}
-	function formatPaysched(psched){
+	function formatPaysched(psched,is_old){
+		let _paysched=[];
 		psched.map(function(sched){
+			
 			sched.disp_amount =  util.formatMoney(sched.due_amount);
 			
 			if(sched.bill_month=="UPONNROL")
 				sched.disp_date = "Upon Enrollment";
 			else
 				sched.disp_date = util.formatDate(new Date(sched.due_date));
+			if(is_old){
+				sched.transaction_type_id ='OLDAC';
+				sched.amount = sched.due_amount;
+			}
+			if(sched.status!=='PAID'){
+				_paysched.push(sched);	
+			}
 			
-			paysched.push(sched);
 		});
+		paysched = angular.copy( _paysched);
 	}
 	function computeAmounts(){
 		paysched.map(function(sched){
 			var ttid=  sched.transaction_type_id;
-			switch(ttid){
-				case 'INIPY':
-					updateAmount(ttid,'set',sched.due_amount);
-					return;
-				break;
-				case 'SBQPY':
-					updateAmount('SBQPY','add',sched.due_amount);
-				break;
-			}
+			var due_date = new Date(sched.due_date);
+			var due_year = due_date.getFullYear();
+			var due_month = due_date.getMonth()+1;
+
+			var today = new Date();
+			var tod_year = today.getFullYear();
+			var tod_month = today.getMonth()+1;
+			
+			var isDue = due_year<tod_year || (due_year==tod_year && due_month<=tod_month);
+			if(!isDue) return;
+				switch(ttid){
+					case 'INIPY':
+						updateAmount(ttid,'set',sched.due_amount);
+						return;
+					break;
+					case 'SBQPY':
+						updateAmount('SBQPY','add',sched.due_amount);
+					break;
+					case 'OLDAC':
+						updateAmount('OLDAC','add',sched.due_amount);
+					break;
+				}
 		});
 	}
 	function defaultList(){
 		updateAmount('INIPY','set',0);
 		updateAmount('SBQPY','set',0);
+		updateAmount('OLDAC','set',0);
 		updateDisplay('INIPY','show');
 		updateDisplay('SBQPY','hide');
+		updateDisplay('OLDAC','hide');
 	}
-
+	function updateDisplays(){
+		list.map(function(lItem){
+			var hasAmt = lItem.amount>0;
+			var disp =hasAmt?'show':'hide';
+			updateDisplay(lItem.id,disp);
+		});
+	}
 	
 	TRNX.runDefault = function(){
 		defaultList();
@@ -97,16 +128,39 @@ define(['app','util','api'],function(app,util) {
 			limit:1,
 			status:'ACTIV'
 		};
-		paysched=[];
+		return TRNX.requestAccount('assessments',filter,'Paysched');
+	}
+
+	TRNX.getAccount = function(sid,sy){
+		var esp = sy + '.25';
+		var filter = {
+			id:sid,
+			limit:1,
+		};
+		updateDisplay('OLDAC','show');
+		return TRNX.requestAccount('accounts',filter,'Paysched');
+	}
+	TRNX.getOldAccount = function(sid,sy){
+		var esp = sy + '.25';
+		var filter = {
+			account_id:sid,
+			limit:1,
+		};
+		return TRNX.requestAccount('payment_plans',filter,'schedule');
+	}
+	TRNX.requestAccount = function(endpoint,filter,field){
 		var success  =function(response){
-			var ASS =  response.data[0];
-			formatPaysched(ASS.Paysched);
+			var data =  response.data[0];
+			var _paysched = data[field];
+			var is_old = endpoint=='payment_plans';
+			formatPaysched(_paysched,is_old);
 			computeAmounts();
+			updateDisplays();
 		};
 		var error  =function(response){
 
 		};
-		return api.GET('assessments',filter, success,error);
+		return api.GET(endpoint,filter,success,error);
 	}
 	return TRNX;
 });
