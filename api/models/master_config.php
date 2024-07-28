e1bd43d1df74fb18700d0456a2bc434d4f825379
Dave A Dev <daveadev@gmail.com>
2024-07-29 00:31:29 +0800

    Add getInfo function in master config

--- api/models/master_config.php
+++ api/models/master_config.php
@@ -3,4 +3,13 @@ class MasterConfig extends AppModel {
 	var $name = 'MasterConfig';
 	var $cacheExpires = '+30 day';
 	var $usePaginationCache = false;
+
+	function getInfo($field=null){
+		$conf = array();
+		$conf['fields'] = array('sys_key','sys_value');
+		if($field)
+			$conf['cond'] = array('sys_key'=>$field);
+		$info = $this->find('list',$conf);
+		return $info;
+	}
 }
