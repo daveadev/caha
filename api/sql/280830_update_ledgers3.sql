aafbf83ef28503d5ae8bb75aea5547312334bc80
Dave A Dev <daveadev@gmail.com>
2024-08-30 08:12:43 +0800

    Ledger for updates 3

---
+++ api/sql/280830_update_ledgers3.sql
@@ -0,0 +1,23 @@
+UPDATE account_schedules
+	SET paid_amount=174
+	WHERE id=2470;
+UPDATE account_schedules
+	SET due_amount=260,paid_amount=260
+	WHERE id=1751;
+UPDATE account_schedules
+	SET due_amount=21242.60,paid_amount=21242.6
+	WHERE id=1750;
+UPDATE ledgers
+	SET ref_no='#10324'
+	WHERE id=1104;
+UPDATE ledgers
+	SET ref_no='#10324'
+	WHERE id=1103;
+UPDATE ledgers
+	SET ref_no='#9394'
+	WHERE id=1102;
+DELETE FROM ledgers
+	WHERE id=1042;
+UPDATE account_schedules
+	SET paid_amount=1000,due_amount=6014.6
+	WHERE id=1531;
