2b764af7df78b2d3ce5b6e1f4cf56c75c586fef4
Dave A Dev <daveadev@gmail.com>
2024-08-30 12:20:29 +0800

    Update G3 classlist

---
+++ api/config/240830_update_marqa.sql
@@ -0,0 +1,26 @@
+INSERT INTO sections (id,department_id,year_level_id,name,description,alias,esp,`order`,created,modified)
+	VALUES (2301,'GS','G3','Turquiose','Turquiose','Turquiose',2024.00,2,'2024-07-28 23:02:11','2024-07-28 23:02:11');
+	UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=95;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=97;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=98;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=100;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=101;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=112;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=113;
+UPDATE classlist_blocks
+	SET section_id=2301
+	WHERE id=114;
