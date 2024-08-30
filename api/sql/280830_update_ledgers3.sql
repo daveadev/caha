a8dc717c029c389ec9a41b84af9c2df42c28cc71
Dave A Dev <daveadev@gmail.com>
2024-08-30 11:47:31 +0800

    Update ledgers 3

--- api/sql/280830_update_ledgers3.sql
+++ api/sql/280830_update_ledgers3.sql
@@ -21,3 +21,17 @@ DELETE FROM ledgers
 UPDATE account_schedules
 	SET paid_amount=1000,due_amount=6014.6
 	WHERE id=1531;
+UPDATE ledgers
+	SET amount=12010
+	WHERE id=965;
+UPDATE account_schedules
+	SET paid_amount=12010
+	WHERE id=1431;
+INSERT INTO ledgers (account_id,`type`,transaction_type_id,esp,transac_date,transac_time,ref_no,details,amount,notes,created)
+	VALUES ('GRS81380','-','DSCNT',2024.00,'2024-07-15','00:34:56','GRA000104','Honor Discount',1980,'','2024-08-03 00:34:56');
+UPDATE account_schedules
+	SET due_amount=250,paid_amount=250
+	WHERE id=1520;
+UPDATE account_schedules
+	SET paid_amount=177
+	WHERE id=2430;
